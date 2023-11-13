3ae0e5cc176252a2062c5775f85578cd718146a7
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-11-13 17:02:00 +0100

    add: btn to change font size

--- js/script.js
+++ js/script.js
@@ -6,7 +6,21 @@ const app = createApp({
     return {
       text: "Hello Vue!",
       img: "edgar-castrejon.jpg",
+      fontSize: " ",
     };
+  },
+  methods: {
+    changeFontSize: function () {
+      if (this.fontSize === " ") {
+        this.fontSize = "f-small";
+      } else if (this.fontSize === "f-small") {
+        this.fontSize = "f-mid";
+      } else if (this.fontSize === "f-mid") {
+        this.fontSize = "f-big";
+      } else {
+        this.fontSize = " ";
+      }
+    },
   },
 });
 
